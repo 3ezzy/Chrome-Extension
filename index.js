50476e187c87b8c92d3736e4bc75ed03c544c920
Mehdi <khammali26@gmail.com>
2025-01-29 16:40:57 +0100

    Add functionality to save current tab URL and update local storage management

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-
-// function Savelead(){
-//     console.log("Save Lead");
-// }
-
-let myleads = []
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("save-btn")
 
 if (leadsFromLocalStorage) {
-    myleads = leadsFromLocalStorage
-    renderLeads(myleads)
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
 }
 
-function renderLeads() {
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+})
+
+function renderLeads(leads) {
     let listItems = ""
-    for (let i = 0; i < myleads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myleads[i]}'>
-                    ${myleads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
 deleteBtn.addEventListener("dblclick", function () {
+    myLeads = []
     localStorage.clear()
-    myleads = []
-    renderLeads()
+    renderLeads(myLeads)
 })
- 
+
 inputBtn.addEventListener("click", function () {
-    myleads.push(inputEl.value)
+    myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myleads", JSON.stringify(myleads))
-    renderLeads()
-    
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
 })
-
-
